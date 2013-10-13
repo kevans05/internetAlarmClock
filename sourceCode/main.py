910edb6f9a877e9055a8da4af804a2e9a01f600f
Kyle Evans <K.evans@gmx.com>
2013-10-13 19:02:13 -0400

    added the ntplib added a function that will check the ntp time vs the hardware time 	if the hardware time is more then a min out it will update to ntp time added a place fro the main function

--- sourceCode/main.py
+++ sourceCode/main.py
@@ -1,24 +1,36 @@
-import ntplib,datetime
-from time import ctime
-
-
-def main():
-
-
-
-
-if __name__ == "__main__":
-main()
+import ntplib,datetime, os
+from time import ctime, localtime, strftime,sleep
 
 
 def compairNTPvsOS():
 	c = ntplib.NTPClient()
-	response = c.request('europe.pool.ntp.org', version=3)
-	if response.offset >= 60:
-		print "NTP TIME:", c
-	else
-		print "Hardware TIME:",datetime.now()
+	response = c.request('0.ca.pool.ntp.org', version=3)
+	if(abs(response.offset) >= 60):
+		print "NTP TIME:", ctime(response.tx_time)
+		os.system('sh ./setRealTimeClock.sh')
+	else:
+		print "Hardware TIME:", strftime("%I:%M:%S", localtime())
+
+def checkAlarm():
+	print "I need an alarm function"
 
 
-compairNTPvsOS( );
+
+def main():
+	compairNTPvsOS()
+	var = 0
+	os.system('clear') 
+	while True:
+		if var >= 3600:
+			var = 0
+			compairNTPvsOS
+		else:
+			checkAlarm()
+			print var
+		var+=1
+		sleep(1)
+		os.system('clear') 
+
+main()
+
 
