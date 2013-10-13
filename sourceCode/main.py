d044850407d537f6516cec09ad483767f291bd0c
Kyle Evans <K.evans@gmx.com>
2013-10-13 12:42:01 -0400

    made one source file for now

--- sourceCode/main.py
+++ sourceCode/main.py
@@ -1,3 +1,5 @@
+import ntplib,datetime
+from time import ctime
 
 
 def main():
@@ -7,3 +9,16 @@ def main():
 
 if __name__ == "__main__":
 main()
+
+
+def compairNTPvsOS():
+	c = ntplib.NTPClient()
+	response = c.request('europe.pool.ntp.org', version=3)
+	if response.offset >= 60:
+		print "NTP TIME:", c
+	else
+		print "Hardware TIME:",datetime.now()
+
+
+compairNTPvsOS( );
+
