7c116522aeeff4c80163636e77b7739ed713d531
Kyle Evans <K.evans@gmx.com>
2013-10-13 20:40:09 -0400

    check to see if internet before doing a ntp check, defaults to rtc if no internet connection

--- sourceCode/main.py
+++ sourceCode/main.py
@@ -1,19 +1,28 @@
-import ntplib,datetime, os
+import ntplib,datetime, os, urllib2
 from time import ctime, localtime, strftime,sleep
 
 
+def internet_on():
+    try:
+        response=urllib2.urlopen('http://74.125.228.100',timeout=1)
+        return True
+    except urllib2.URLError as err: pass
+    return False
+
 def compairNTPvsOS():
-	c = ntplib.NTPClient()
-	response = c.request('0.ca.pool.ntp.org', version=3)
-	if(abs(response.offset) >= 60):
-		print "NTP TIME:", ctime(response.tx_time)
-		os.system('sh ./setRealTimeClock.sh')
+	if internet_on() == True:
+		c = ntplib.NTPClient()
+		response = c.request('0.ca.pool.ntp.org', version=3)
+		if(abs(response.offset) >= 60):
+			print "NTP TIME:", ctime(response.tx_time)
+			os.system('sh ./setRealTimeClock.sh')
+		else:
+			print "Hardware TIME:", strftime("%I:%M:%S", localtime())
 	else:
-		print "Hardware TIME:", strftime("%I:%M:%S", localtime())
+		print "NTP CHECK FAILED \n\n Hardware TIME:", strftime("%I:%M:%S", localtime())ß
 
 def checkAlarm():
 	print "I need an alarm function"
-
 
 
 def main():
