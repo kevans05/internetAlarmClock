5d693a58507d659742b1903d7a50d014d58c6162
Kyle Evans <K.evans@gmx.com>
2013-10-19 10:01:11 -0400

    working at separating the cdv file so it can be called separately. Also trying to keep the cdv file constant for the check

--- sourceCode/main.py
+++ sourceCode/main.py
@@ -1,7 +1,6 @@
 import ntplib,datetime, os, urllib2, csv, webbrowser
 from time import ctime, localtime, strftime,sleep
 
-
 def internetOn():
     try:
         response=urllib2.urlopen('http://74.125.228.100',timeout=1)
@@ -24,26 +23,40 @@ def compairNTPvsOS():
 
 def checkAlarm(now):
 	print "I need an alarm function"
-	print "Next Alarm", alarmTimeData
-	if now == alarmTimeData:
-		webbrowser.open(webAddress)
-
-def pullOnCSVTime():
+	#print "Next Alarm", alarmTimeData
+	#if now == alarmTimeData:
+		#webbrowser.open(webAddress)
+def pullCSV():
 	TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 	with open('egg.csv', 'rU') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-		rownum = 0
-		try:
-			for row in spamreader:
-				if(rownum == 1):
-					alarmTimeData = datetime.datetime.strptime(row[0], '%m/%d/%Y/%H:%M')
-					print alarmTimeData
-				elif(rownum == 2):
-					webAddress = row[0]
-					#print webAddress
-				rownum += 1
-		except csv.Error as e:
-			sys.exit()
+		return csvfile
+
+		#spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+		#rownum = 0
+		#try:
+		#	for row in spamreader:
+		#		if(rownum == 1):
+		#			alarmTimeData = datetime.datetime.strptime(row[0], '%m/%d/%Y/%H:%M')
+		#			print alarmTimeData
+		#		elif(rownum == 2):
+		#			webAddress = row[0]
+		#			print webAddress
+		#		rownum += 1
+		#except csv.Error as e:
+		#	sys.exit()
+def getTimeFromCSV(csvfile):
+	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+	rownum = 0
+	try:
+		for row in spamreader:
+			if(rownum == 1):
+				return datetime.datetime.strptime(row[0], '%m/%d/%Y/%H:%M')
+			else:
+				return Error
+			rownum+=rownum
+	except Exception, e:
+		raise e
+		return Error
 
 def main():
 	compairNTPvsOS()
@@ -55,7 +68,8 @@ def main():
 			var = 0
 			compairNTPvsOS
 		elif  var%60 == 0:
-			pullOnCSV()
+			csvFile = pullCSV()
+			getTimeFromCSV(csvFile)
 		else:
 			checkAlarm(now)
 			y = strftime("%H:%M:%S", localtime())
