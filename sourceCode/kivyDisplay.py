2d9edb6660f56b8af00aba5836a728b4d0fb05f2
Kyle Evans <k.evans@gmx.com>
2014-02-10 15:09:41 -0500

    I got bored of waiting for adam and stated to make a GUI. it is super simple right now and only gets the time how ever I am going to make an alarm class in it tonight. Currently Displays Time Date can change size and location.

---
+++ sourceCode/kivyDisplay.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.clock import Clock
+
+from time import ctime, localtime, strftime,sleep
+
+class timeDisplay(Label):
+    def update(self, *args):
+    	self.font_size = '100dp'
+    	self.halign = 'left'
+    	#self.color=[0, 0, 8, 1]
+    	self.padding_y = 175
+        self.text = strftime("%I:%M:%S %p", localtime()) + "\n" + strftime("%d-%B-%Y", localtime())
+
+class TimeApp(App):
+    def build(self):
+        crudeclock = timeDisplay()
+        Clock.schedule_interval(crudeclock.update, 1)
+        return crudeclock
+
+if __name__ == "__main__":
+    TimeApp().run()
